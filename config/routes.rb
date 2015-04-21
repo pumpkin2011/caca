f35d742571a8a94902bed5827b597319986ed6d9
guxiaobai <sikuan.gu@gmail.com>
2015-04-21 23:04:45 +0800

    feature@admin root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  
+  namespace :admin do
+    root 'welcome#index'
+  end
+
   devise_for :users
 
   # Example of regular route:
