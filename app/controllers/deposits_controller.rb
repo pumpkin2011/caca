700b1885cdcdcf1c87aae8dfd7ea584bf41cb65d
guxiaobai <sikuan.gu@gmail.com>
2015-04-24 01:51:13 +0800

    用户充值系统调整
    * 取消日志记录
    * 统一异常处理

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -6,14 +6,10 @@ class DepositsController < ApplicationController
 
   #TODO: 机器恶意充值处理
   def create
-    @depost = Deposit.where( deposit_param ).pending.take
-    begin
-      if @depost.update(user: current_user)
-        logger.info "[success][#{Time.now}][deposits][#{current_user.id}]: #{deposit_param[:sn]}-#{deposit_param[:amount]}"
-        flash[:success] = "充值成功"
-      end
-    rescue Exception => e
-      logger.error "[error][#{Time.now}][deposits][#{current_user.id}]: #{deposit_param[:sn]}-#{deposit_param[:amount]}"
+    @depost = Deposit.pending.find_by!(deposit_param)
+    if @depost.update(user: current_user)
+      flash[:success] = "充值成功"
+    else
       flash[:error] = "充值失败"
     end
     redirect_to deposits_path
