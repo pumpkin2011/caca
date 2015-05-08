b70d425e9eb684048b8d9a92776200008ff8e403
guxiaobai <sikuan.gu@gmail.com>
2015-05-08 21:27:02 +0800

    首页为任务大厅

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'tasks#index'
   get '/profile'=> 'welcome#profile', as: :profile
   get '/profile/:id' => 'users#profile'
   post '/frozen' => 'welcome#frozen'
