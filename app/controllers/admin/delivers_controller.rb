386ea81cad20dfe6cd1994bc657120f84be60e3c
guxiaobai <sikuan.gu@gmail.com>
2015-05-16 00:41:20 +0800

    用户收货地址

--- app/controllers/admin/delivers_controller.rb
+++ app/controllers/admin/delivers_controller.rb
@@ -1,14 +1,17 @@
 class Admin::DeliversController < ApplicationController
 
   def index
+    @count = Deliver.count
     @pending_count = Deliver.pending.count
     @confirmed_count = Deliver.confirmed.count
     @rejected_count = Deliver.rejected.count
     @applying_count = Deliver.applying.count
-    unless %w(pending confirmed rejected applying).include?( params[:type] )
-      params[:type] = 'pending'
+    if %w(pending confirmed rejected applying).include?( params[:type] )
+      @delivers = Deliver.page(params[:page]).send(params[:type].to_sym)
+    else
+      @delivers = Deliver.page(params[:page]).per(2)
     end
-    @delivers = Deliver.send(params[:type].to_sym)
+
   end
 
 
@@ -16,14 +19,14 @@ class Admin::DeliversController < ApplicationController
   def reject
     Deliver.pending.find( params[:id] ).reject!
     flash[:success] = '地址审核成功: 拒绝'
-    redirect_to admin_delivers_path
+    redirect_to :back
   end
 
   # 地址审核: 通过
   def confirm
     Deliver.pending.find( params[:id] ).confirm!
     flash[:success] = '地址审核成功: 同意'
-    redirect_to admin_delivers_path
+    redirect_to :back
   end
 
 end
