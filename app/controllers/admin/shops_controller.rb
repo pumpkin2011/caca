2c331dac194a228c9578b1e64e247ecba7a46345
guxiaobai <sikuan.gu@gmail.com>
2015-05-15 23:58:01 +0800

    掌柜分页系统

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -1,24 +1,27 @@
 class Admin::ShopsController < ApplicationController
   def index
+    @count = Shop.count
     @pending_count = Shop.pending.count
     @confirmed_count = Shop.confirmed.count
     @rejected_count = Shop.rejected.count
 
-    unless %w(pending confirmed rejected).include?( params[:type] )
-      params[:type] = 'pending'
+    if %w(pending confirmed rejected).include?( params[:type] )
+      @shops = Shop.page(params[:page]).send(params[:type].to_sym)
+    else
+      @shops = Shop.page(params[:page])
     end
-    @shops = Shop.send(params[:type].to_sym)
+
   end
 
   def reject
     Shop.pending.find( params[:id] ).reject!
     flash[:success] = '店铺审核成功: 拒绝'
-    redirect_to admin_shops_path
+    redirect_to :back
   end
 
   def confirm
     Shop.pending.find( params[:id] ).confirm!
     flash[:success] = '店铺审核成功: 同意'
-    redirect_to admin_shops_path
+    redirect_to :back
   end
 end
