bccc26d4bd045127c2252f6be2e962d91f7921bc
guxiaobai <sikuan.gu@gmail.com>
2015-05-16 00:44:27 +0800

    店铺任务统计

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -18,6 +18,7 @@
 class Shop < ActiveRecord::Base
   include AASM
   belongs_to :user
+  has_many :tasks
 
   validates_presence_of :account
   validates :account, length: { minimum: 3, maximum: 25},
@@ -35,6 +36,18 @@ class Shop < ActiveRecord::Base
     event :confirm do
       transitions from: :pending, to: :confirmed
     end
+  end
+
+  def day_count
+    self.tasks.where('created_at > ?', 1.days.ago).count
+  end
+
+  def week_count
+    self.tasks.where('created_at > ?', 7.days.ago).count
+  end
+
+  def month_count
+    self.tasks.where('created_at > ?', 30.days.ago).count
   end
 
   validate :shops_count_within_limit, on: :create
