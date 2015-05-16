f83f05220864719475d231846481f1068b23f821
guxiaobai <sikuan.gu@gmail.com>
2015-05-16 22:51:00 +0800

    任务获取用户信息

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   include DeliversHelper
   before_action :authenticate_user!
-  
+
   def profile
 
     # 真实姓名
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
     @task = Task.find(params[:id])
 
     render json: {
-      name: @task.order.user.identity.name,
-      wangwang: @task.order.wangwang.account,
-      deliver_name: @task.order.user.deliver.name,
-      deliver_address: full_address(@task.order.user.deliver)
+      name: @task.consumer.name,
+      wangwang: @task.wangwang.account,
+      deliver_name: @task.consumer.deliver.name,
+      deliver_address: full_address(@task.consumer.deliver)
     }
   end
 end
