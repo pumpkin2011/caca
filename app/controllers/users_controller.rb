4a6b1c1bc3c49b214f06cfb0e52afb52a8e4a00b
guxiaobai <sikuan.gu@gmail.com>
2015-06-06 00:29:10 +0800

    查看刷手信息增加支付宝信息

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,10 @@ class UsersController < ApplicationController
       name: @task.consumer.name,
       wangwang: @task.wangwang.account,
       deliver_name: @task.consumer.deliver.name,
-      deliver_address: full_address(@task.consumer.deliver)
+      deliver_address: full_address(@task.consumer.deliver),
+      order_count: @task.consumer.orders.finished.count,
+      alipay_name: @task.consumer.alipay.name,
+      alipay_account: @task.consumer.alipay.account
     }
   end
 end
