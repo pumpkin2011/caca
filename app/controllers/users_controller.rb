b5297dc10e58e65983e1227a3e409f8af2d9813d
guxiaobai <sikuan.gu@gmail.com>
2015-06-15 01:15:10 +0800

    bump取消地址交换

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
     # 支付宝
     # 收获地址
     @task = Task.find(params[:id])
-
+    deliver = @task.consumer.delivers.confirmed.first
     render json: {
       name: @task.consumer.name,
       wangwang: @task.wangwang.account,
-      deliver_name: @task.consumer.deliver.name,
-      deliver_address: full_address(@task.consumer.deliver),
+      deliver_name: deliver.name,
+      deliver_address: full_address(deliver),
       order_count: @task.consumer.orders.finished.count,
       alipay_name: @task.consumer.alipay.name,
       alipay_account: @task.consumer.alipay.account
