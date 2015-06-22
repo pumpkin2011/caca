35259ec7d5968f2d5e7e38ae0f08694ffa15274d
guxiaobai <sikuan.gu@gmail.com>
2015-06-22 12:49:52 +0800

    统计数据

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -1,4 +1,15 @@
 class Admin::WelcomeController < ApplicationController
   def index
+    # 用户
+    @all_user_count = User.all.count
+    @locked_user_count = User.where.not(locked_at: nil).count
+
+    #  佣金
+    @commission_total = Task.sum(:commission)
+    @commission_finished = Task.finished.sum(:commission)
+
+    # 充值
+    @deposit_total = Deposit.sum(:amount)
+    @deposit_applying = Deposit.applying.sum(:amount)
   end
 end
